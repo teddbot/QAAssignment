f58d19355d1e5c64d4aa373d4ea9c41e1bdcecca
Teddy Nzioka <nziokateddy@gmail.com>
2024-05-19 17:29:03 +0300

    Debugging workflow.

--- Resources/API/TestData/Testdata.py
+++ Resources/API/TestData/Testdata.py
@@ -16,15 +16,18 @@ def get_environment(self, environment):
     else:
         raise Exception("environment not found")
 
+
 class UAT:
     Url = ""
     USERNAME = ""
     PASSWORD = ""
 
+
 class SIT:
     Url = ""
     USERNAME = ""
     PASSWORD = ""
+
 
 class PROD:
     Url = ""
@@ -33,92 +36,92 @@ class PROD:
 
 
 # NODE_URL= "http://localhost:8545"
-NODE_URL= "http://139.144.26.89:4000"
+NODE_URL = "http://139.144.26.89:4000"
 
 # JSON_RPC_VERIFICATION
 blockNumber = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_blockNumber",
-      "params": []
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_blockNumber",
+    "params": []
+}
 earliestBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": ["earliest", True]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": ["earliest", True]
+}
 finalizedBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": ["finalized", True]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": ["finalized", True]
+}
 latestBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": ["latest", True]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": ["latest", True]
+}
 pendingBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": ["pending", True]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": ["pending", True]
+}
 safeBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": ["safe", True]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": ["safe", True]
+}
 invalidBlockParams = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "eth_getBlockByNumber",
-      "params": []
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "eth_getBlockByNumber",
+    "params": []
+}
 invalidBlockMethod = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "",
-      "params": ["earliest", False]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "",
+    "params": ["earliest", False]
+}
 
 # TRACE_ NAMESPACE
 earliestTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": [0]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": [0]
+}
 finalizedTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": [0]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": [0]
+}
 latestTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": [0]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": [0]
+}
 pendingTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": [0]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": [0]
+}
 safeTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": [0]
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": [0]
+}
 invalidTraceBlock = {
-      "jsonrpc": "2.0",
-      "id": 0,
-      "method": "trace_block",
-      "params": []
-    }
+    "jsonrpc": "2.0",
+    "id": 0,
+    "method": "trace_block",
+    "params": []
+}
